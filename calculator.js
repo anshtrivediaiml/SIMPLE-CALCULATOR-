498f9140b35b0526c8bf1330d2f6d8851c099dca
anshtrivediaiml <22aiml056@charusat.edu.in>
2024-03-17 14:25:51 +0530

    calculator

--- calculator.js
+++ calculator.js
@@ -1,29 +1,24 @@
-let input= document.getElementById('inputBox');
-let buttons= document.querySelectorAll('button');
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
 
-let string="";
-buttons.forEach((element)=>{
-element.addEventListener('click',(b)=>{
-    if(b.target.innerText== '=')
-    {
-     string= String(eval(string)); //Used to evaluate the expression the string 
-     inputBox.value=string;
+let string = "";
+buttons.forEach((element) => {
+  element.addEventListener("click", (b) => {
+    if (b.target.innerText == "=") {
+      string = String(eval(string)); //Used to evaluate the expression the string
+      inputBox.value = string;
+    } else if (b.target.innerText == "AC") {
+      string = "";
+      inputBox.value = string;
+    } else if (b.target.innerText == "DEL") {
+      string = string.substring(0, string.length - 1);
+      inputBox.value = string;
+    } else if (b.target.id == "plusMinus") {
+      string = String(-eval(string));
+      inputBox.value = string;
+    } else {
+      string += b.target.innerText;
+      inputBox.value = string;
     }
-
-    else if(b.target.innerText=='AC')
-    {
-        string='';
-        inputBox.value=string;
-    }
-
-    else if(b.target.innerText=='DEL')
-    {
-        string =string.substring(0,string.length-1);
-        inputBox.value=string;
-    }
-    else{
-        string+=b.target.innerText;
-        inputBox.value= string;
-    }
-})
-})
+  });
+});
