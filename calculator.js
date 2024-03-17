6259824ed66300e65391db4177e4c166bba22469
anshtrivediaiml <22aiml056@charusat.edu.in>
2024-03-17 14:16:25 +0530

    calculator

---
+++ calculator.js
@@ -0,0 +1,29 @@
+let input= document.getElementById('inputBox');
+let buttons= document.querySelectorAll('button');
+
+let string="";
+buttons.forEach((element)=>{
+element.addEventListener('click',(b)=>{
+    if(b.target.innerText== '=')
+    {
+     string= String(eval(string)); //Used to evaluate the expression the string 
+     inputBox.value=string;
+    }
+
+    else if(b.target.innerText=='AC')
+    {
+        string='';
+        inputBox.value=string;
+    }
+
+    else if(b.target.innerText=='DEL')
+    {
+        string =string.substring(0,string.length-1);
+        inputBox.value=string;
+    }
+    else{
+        string+=b.target.innerText;
+        inputBox.value= string;
+    }
+})
+})
